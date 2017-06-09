3487966d6c2d75ff586a9243e197475db6c50bf4
Christian Scheil <Christian.Scheil@icloud.com>
2017-06-09 16:35:55 +0200

    Stand aus dem Workshop

--- src/main/java/parkRechner/ParkRechner.java
+++ src/main/java/parkRechner/ParkRechner.java
@@ -8,6 +8,8 @@ import java.time.temporal.ChronoUnit;
  */
 public class ParkRechner {
 
+    private static final int PREIS_PRO_STUNDE = 2;
+
     public int parke(final Instant startDate, final Instant endDate){
         final long fullTimeInDays = ChronoUnit.DAYS.between(startDate, endDate);
         final long weeks = fullTimeInDays/7;
@@ -19,8 +21,27 @@ public class ParkRechner {
         newDate = newDate.minus(hours,ChronoUnit.HOURS);
         final long minutes = ChronoUnit.MINUTES.between(startDate, newDate);
 
-        int result = 2;
+        int result = PREIS_PRO_STUNDE;
+        if (hours == 0) {
+            result = PREIS_PRO_STUNDE;
+        } else if ((hours <= 5)&&(hours > 0)) {
+            result = berechnePreisFuerStunden((int) hours);
+        } else {
+            result = 10;
+        }
 
         return result;
+    }
+
+    private int berechnePreisFuerStunden(int hours) {
+
+        int preis = 0;
+
+        if (hours == 0) {
+            preis = PREIS_PRO_STUNDE;
+        } else {
+            preis = hours * PREIS_PRO_STUNDE;
+        }
+        return preis;
     }
 }
--- src/test/java/junit/ParkRechnerTest.java
+++ src/test/java/junit/ParkRechnerTest.java
@@ -1,5 +1,8 @@
 package junit;
 
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.Before;
 import parkRechner.ParkRechner;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,13 +14,60 @@ import java.time.Instant;
  */
 public class ParkRechnerTest {
 
+    private ParkRechner parkRechner;
+    private Instant startTime;
+
+    @Before
+    public void setUp() throws Exception {
+        this.parkRechner = new ParkRechner();
+        this.startTime = Instant.parse("2017-03-01T00:00:00Z");
+    }
+
     @Test
-    public void testParkeUnter1h(){
-        ParkRechner parkRechner = new ParkRechner();
-        Instant startTime = Instant.parse("2017-03-01T00:00:00Z");
-        Instant endTime = Instant.parse("2017-03-01T00:44:00Z");
-        final int payment = parkRechner.parke(startTime,endTime);
-        Assert.assertEquals(2,payment);
+    public void testParkeUnter1h() {
+        final Instant endTime = Instant.parse("2017-03-01T00:44:00Z");
+        final int payment = parkRechner.parke(startTime, endTime);
+        Assert.assertEquals(2, payment);
+    }
+
+    @Test
+    public void testParkeFuerGenauEineStunde() throws Exception {
+        final Instant endTime = Instant.parse("2017-03-01T01:00:00Z");
+        final int payment = parkRechner.parke(startTime, endTime);
+        Assert.assertEquals(2, payment);
+    }
+    @Test
+    public void testParkeFuerDreiStunden() throws Exception {
+        final Instant endTime = Instant.parse("2017-03-01T03:00:00Z");
+        final int payment = parkRechner.parke(startTime, endTime);
+        Assert.assertEquals(6, payment);
+    }
+    @Test
+     public void testParkeFuerFuenfStunden() throws Exception {
+        final Instant endTime = Instant.parse("2017-03-01T05:00:00Z");
+        final int payment = parkRechner.parke(startTime, endTime);
+        Assert.assertEquals(10, payment);
+    }
+
+    @Test
+    public void testParkeFuerSechsStunden() throws Exception {
+        final Instant endTime = Instant.parse("2017-03-01T06:00:00Z");
+        final int payment = parkRechner.parke(startTime, endTime);
+        Assert.assertEquals(10, payment);
+    }
+
+    //@Test
+    public void testParkeFuerVierundzwanzigStunden() throws Exception {
+        final Instant endTime = Instant.parse("2017-03-02T00:00:00Z");
+        final int payment = parkRechner.parke(startTime, endTime);
+        Assert.assertEquals(10, payment);
+    }
+
+    //@Test
+    public void testParkeFuerEinTag1Stunde() throws Exception {
+        final Instant endTime = Instant.parse("2017-03-02T01:00:00Z");
+        final int payment = parkRechner.parke(startTime, endTime);
+        Assert.assertEquals(12, payment);
     }
 
 }
